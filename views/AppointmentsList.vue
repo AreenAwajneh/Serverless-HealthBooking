1ac9ffb3579ffd9b8988c83d2ddda772e089fed7
Areen <areenawajneh@gmail.com>
2025-06-19 17:20:18 +0300

    update

--- views/AppointmentsList.vue
+++ views/AppointmentsList.vue
@@ -59,7 +59,7 @@ export default {
   },
   methods: {
     fetchAppointments() {
-      fetch("https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments")
+      fetch("https://tlx7wgi46i.execute-api.us-east-2.amazonaws.com/Health/Appointments")
         .then(res => res.json())
         .then(data => {
           const parsed = JSON.parse(data.body);
@@ -74,7 +74,7 @@ export default {
       console.log("appointmentId:", cleanAppointment.appointmentId);
       console.log(" appointmentId (direct):", appointment.appointmentId);
 
-      const url = `https://e2m2b7y8c9.execute-api.us-east-1.amazonaws.com/prod/appointments/${appointment.appointmentId}`;
+      const url = `https://tlx7wgi46i.execute-api.us-east-2.amazonaws.com/Health/Appointments/${Appointments.appointmentId}`;
 
       const payload = { status: newStatus };
 
